7ba2a14f1a70e497a32b21dd40c71583320f031c
Bartek Kroczek <bartek.kroczek@doctoral.uj.edu.pl>
2020-08-01 15:05:44 +0100

    EEG and ET synchro done

--- synchro_skeleton.py
+++ synchro_skeleton.py
@@ -21,21 +21,20 @@ FIO1 = 6701  # the address of line FIO1 (EEG)
 
 
 # GLOBALS
-DEBUG = False
-STIM_SIZE = 40
-VISUAL_OFFSET = 60
-STIM_COLOR = '#f2f2f2'
+DEBUG = True
+TEXT_SIZE = 30
+VISUAL_OFFSET = 90
 KEYS = ['left', 'right']
 
 RESULTS = list()
-RESULTS.append(
-    ['PART_ID', 'Trial', 'Stimuli', 'Version', 'Training', 'FIXTIME', 'TIME','Correct', 
-	'SOA','Reversal', 'Reversal_count', 'Latency', 'stim_name', 'Rating'])
+RESULTS.append(['PART_ID', 'Trial', 'Stimuli', 'Training', 'FIXTIME', 'MTIME', 'Correct', 'SOA',
+                'Level', 'Reversal', 'Reversal_count', 'Latency', 'Rating'])
 
 
 class CorrectStim(object):  # Correct Stimulus Enumerator
     LEFT = 1
     RIGHT = 2
+
 
 class Triggers(object):
     CLEAR = 0xFF00
@@ -45,14 +44,9 @@ class Triggers(object):
     REACTION = 0xFF08
     FINISHED = 0xFF10
 
-class QuestonVersion(object):
-    FIRST_SHOWED = 'First_showed'
-    FIRST_HIDDEN = 'First_hidden'
-
-
-@atexit.register
+# @atexit.register
 def save_beh_results():
-    with open(join('results', 'beh', PART_ID + '_' + str(random.choice(range(100, 1000))) + '_beh.csv'), 'w') as beh_file:
+    with open(join('results', 'beh', PART_ID + "_" + str(random.choice(range(100, 1000))) + '_beh.csv'), 'w') as beh_file:
         beh_writer = csv.writer(beh_file)
         beh_writer.writerows(RESULTS)
     logging.flush()
@@ -84,7 +78,8 @@ def read_text_from_file(file_name, insert=''):
 def check_exit(key='f7'):
     stop = event.getKeys(keyList=[key])
     if stop:
-        abort_with_error('Experiment finished by user! {} pressed.'.format(key))
+        abort_with_error(
+            'Experiment finished by user! {} pressed.'.format(key))
 
 
 def show_info(win, file_name, insert=''):
@@ -94,12 +89,15 @@ def show_info(win, file_name, insert=''):
     :return:
     """
     msg = read_text_from_file(file_name, insert=insert)
-    msg = visual.TextStim(win, color=STIM_COLOR, text=msg, height=STIM_SIZE - 10, wrapWidth=SCREEN_RES[0])
+    msg = visual.TextStim(win, color='grey', text=msg,
+                          height=TEXT_SIZE - 10, wrapWidth=SCREEN_RES[0])
     msg.draw()
     win.flip()
-    key = event.waitKeys(keyList=['f7', 'return', 'space', 'left', 'right'] + KEYS)
+    key = event.waitKeys(
+        keyList=['f7', 'return', 'space', 'left', 'right'] + KEYS)
     if key == ['f7']:
-        abort_with_error('Experiment finished by user on info screen! F7 pressed.')
+        abort_with_error(
+            'Experiment finished by user on info screen! F7 pressed.')
     win.flip()
 
 
@@ -115,14 +113,13 @@ def main():
     dictDlg = gui.DlgFromDict(dictionary=info, title='Czas detekcji wzrokowej')
     if not dictDlg.OK:
         abort_with_error('Info dialog terminated.')
-
     PART_ID = info['IDENTYFIKATOR'] + info[u'P\u0141EC'] + info['WIEK']
 
     if DEBUG:
         tk = pylink.EyeLink(None)  # Simulation mode
     else:
         tk = pylink.EyeLink('100.1.1.1')
-    
+
     port.writeRegister(6751, 0xFFFF)  # set FIO1 as output
     port.writeRegister(FIO1, Triggers.CLEAR)  # start low
 
@@ -130,11 +127,10 @@ def main():
     tk.openDataFile(dataFileName)
     # add personalized data file header (preamble text)
     tk.sendCommand(
-        f"add_file_preamble_text 'Temporal Order EyeTracking EXP PART_ID: {PART_ID}'")
-
+        f"add_file_preamble_text 'Inspection Time EyeTracking EXP PART_ID: {PART_ID}'")
 
     # === Scene init ===
-    
+    # we need to set monitor parameters to use the different PsychoPy screen "units"
     mon = monitors.Monitor('myMonitor', width=53.0, distance=100.0)
     mon.setSizePix(SCREEN_RES)
     win = visual.Window(SCREEN_RES, fullscr=(not DEBUG), monitor=mon, color='black',
@@ -176,15 +172,19 @@ def main():
         tvstr = tk.getTrackerVersionString()
         vindex = tvstr.find("EYELINK CL")
         software_ver = float(tvstr.split()[-1])
-    
-    event.Mouse(visible=False, newPos=None, win=win)  # Make mouse invisible
-    logging.LogFile(join('results','log', f'{PART_ID}.log'), level=logging.INFO)  # errors logging
-    logging.info(f'FRAME RATE: {FRAME_RATE}')
-    logging.info(f'SCREEN RES: {SCREEN_RES}')
 
-    pos_feedb = visual.TextStim(win, text=u'Poprawna odpowied\u017A', color=STIM_COLOR, height=40)
-    neg_feedb = visual.TextStim(win, text=u'Niepoprawna odpowied\u017A', color=STIM_COLOR, height=40)
-    no_feedb = visual.TextStim(win, text=u'Nie udzieli\u0142e\u015B odpowiedzi', color=STIM_COLOR, height=40)
+    event.Mouse(visible=False, newPos=None, win=win)  # Make mouse invisible
+    logging.LogFile(join('.', 'results', 'log',
+                         f"{PART_ID}_{str(random.choice(range(100, 1000)))}.log"), level=logging.INFO)  # errors logging
+    logging.info('FRAME RATE: {}'.format(FRAME_RATE))
+    logging.info('SCREEN RES: {}'.format(SCREEN_RES))
+    pos_feedb = visual.TextStim(
+        win, text=u'Poprawna odpowied\u017A', color='grey', height=40)
+    neg_feedb = visual.TextStim(
+        win, text=u'Niepoprawna odpowied\u017A', color='grey', height=40)
+    no_feedb = visual.TextStim(
+        win, text=u'Nie udzieli\u0142e\u015B odpowiedzi', color='grey', height=40)
+    show_info(win, join('.', 'messages', 'hello.txt'))
 
     # sample and event data saved in EDF data file
     tk.sendCommand(
@@ -213,51 +213,48 @@ def main():
     event.waitKeys()
     tk.doTrackerSetup()
 
-
-    for proc_version in ['SQUARES', 'CIRCLES']: 
-        if proc_version == 'SQUARES':
-            left_stim = visual.Rect(win, width=2 * STIM_SIZE, height=2 * STIM_SIZE, lineColor=STIM_COLOR,
-                                    fillColor=STIM_COLOR, pos=(-1 * VISUAL_OFFSET, 0))
-            right_stim = visual.Rect(win, width=2 * STIM_SIZE, height=2 * STIM_SIZE, lineColor=STIM_COLOR,
-                                     fillColor=STIM_COLOR, pos=(1 * VISUAL_OFFSET, 0))
-            question = u'Kt\u00F3ry kwadrat pojawi\u0142 si\u0119 pierwszy?'
-            version = QuestonVersion.FIRST_SHOWED
-
-        elif proc_version == 'CIRCLES':
-            left_stim = visual.Circle(win, radius=1 * STIM_SIZE, lineColor=STIM_COLOR, fillColor=STIM_COLOR,
-                                      pos=(-1 * VISUAL_OFFSET, 0))
-            right_stim = visual.Circle(win, radius=1 * STIM_SIZE, lineColor=STIM_COLOR, fillColor=STIM_COLOR,
-                                       pos=(1 * VISUAL_OFFSET, 0))
-            question = u'Kt\u00F3re k\u00F3\u0142ko znikne\u0142o pierwsze?'
-            version = QuestonVersion.FIRST_HIDDEN
-
-        else:
-            raise NotImplementedError('Procedures working only with Squares or Circles')
-
-        # fix_stim = visual.TextStim(win, text='+', height=100, color=STIM_COLOR)
-        fix_stim = visual.ImageStim(win, image=join('.', 'stims', 'PRE_STIMULI.bmp'))
-        arrow_label = visual.TextStim(win, text=u"\u2190       \u2192", color=STIM_COLOR, height=30,
+    for proc_version in ['SQUARES', 'CIRCLES']:
+        left_stim = visual.ImageStim(win, image=join(
+            '.', 'stims', f'{proc_version}_LEFT.bmp'))
+        right_stim = visual.ImageStim(win, image=join(
+            '.', 'stims', f'{proc_version}_RIGHT.bmp'))
+        mask_stim = visual.ImageStim(win, image=join(
+            '.', 'stims', f'{proc_version}_MASK.bmp'))
+        # fix_stim = visual.TextStim(win, text='+', height=100, color='grey')
+        fix_stim = visual.ImageStim(
+            win, image=join('.', 'stims', 'PRE_STIMULI.bmp'))
+        arrow_label = visual.TextStim(win, text=u"\u2190       \u2192", color='grey', height=30,
                                       pos=(0, -200))
+        if proc_version == 'SQUARES':
+            question = u'Gdzie pojawi\u0142 si\u0119 OBROCONY kwadrat?'
+        elif proc_version == 'CIRCLES':
+            question = u'Gdzie pojawi\u0142 si\u0119 WI\u0118KSZY okr\u0119g?'
+        else:
+            raise NotImplementedError(
+                f'Stimulus type: {proc_version} not implemented.')
 
-        question_text = visual.TextStim(win, text=question, color=STIM_COLOR, height=20,
-                                      pos=(0, -180))
+        question_text = visual.TextStim(win, text=question, color='grey', height=20,
+                                        pos=(0, -180))
 
         # === Load data, configure log ===
 
         response_clock = core.Clock()
-        conf = yaml.load(open(join('.', 'configs' , f'{proc_version}_config.yaml')))
+        conf = yaml.load(
+            open(join('.', 'configs', f'{proc_version}_config.yaml')))
 
-        show_info(win, join('.', 'messages', f'{proc_version}_before_training.txt'))
+        show_info(win, join('.', 'messages',
+                            f'{proc_version}_before_training.txt'))
         # === Training ===
 
-        training = NUpNDown(start_val=conf['START_SOA'], max_revs=conf['MAX_REVS'])
+        training = NUpNDown(
+            start_val=conf['START_SOA'], max_revs=conf['MAX_REVS'])
 
         old_rev_count_val = -1
         correct_trials = 0
         soas = []
         for idx, soa in enumerate(training):
-            corr, rt, stim_name,rating = run_trial(conf, version, fix_stim, left_stim, right_stim, soa, win, arrow_label,
-                                            question_text, response_clock, idx, tk)
+            corr, rt, rating = run_trial(conf, fix_stim, left_stim, mask_stim, right_stim, soa, win, arrow_label,
+                                         question_text, response_clock, idx, tk)
             training.set_corr(corr)
             level, reversal, revs_count = map(int, training.get_jump_status())
             if reversal:
@@ -269,13 +266,13 @@ def main():
                 rev_count_val = '-'
 
             RESULTS.append(
-                [PART_ID, idx, proc_version, version, 1, conf['FIXTIME'], conf['TIME'], int(corr), soa,
-                 reversal,
-                 rev_count_val, rt, stim_name, rating])
+                [PART_ID, idx, proc_version, 1, conf['FIXTIME'], conf['MTIME'], int(corr), soa, level, reversal,
+                 rev_count_val, rt, rating])
 
-            ### FEEDBACK
+            # FEEDBACK
             if corr == 1:
                 feedb_msg = pos_feedb
+                correct_trials += 1
             elif corr == 0:
                 feedb_msg = neg_feedb
             else:
@@ -284,26 +281,21 @@ def main():
                 feedb_msg.draw()
                 check_exit()
                 win.flip()
-            # break + jitter
-            wait_time_in_secs = 1 + random.choice(range(0, 120))/ 60.0
-            core.wait(wait_time_in_secs)
 
+        # === experiment ===
+        soa = int(np.mean(soas[:int(0.6 * len(soas))]))
 
-        # === Experiment ===
-        soa = int(np.mean(soas[:int(0.6 * len(soas))])) 
         show_info(win, join('.', 'messages', f'{proc_version}_feedback.txt'))
 
-        for idx in range(idx, conf['NO_TRIALS']+ idx):
-            corr, rt, stim_name, rating = run_trial(conf, version, fix_stim, left_stim, right_stim, soa, win, arrow_label,
-                                            question_text, response_clock, idx, tk)
-            RESULTS.append([PART_ID, idx, proc_version, version, 0, conf['FIXTIME'], conf['TIME'], corr, soa, '-','-', rt, stim_name, rating])
+        for idx in range(idx, conf['NO_TRIALS']+idx):
+            corr, rt, rating = run_trial(conf, fix_stim, left_stim, mask_stim, right_stim, soa, win, arrow_label,
+                                         question_text, response_clock, idx, tk)
+            corr = int(corr)
+            correct_trials += corr
+            RESULTS.append(
+                [PART_ID, idx, proc_version, 0, conf['FIXTIME'], conf['MTIME'], corr, soa, '-', '-',
+                    '-', rt, rating])
 
-            # break + jitter
-            wait_time_in_secs = 1 + random.choice(range(0, 120))/ 60.0
-            core.wait(wait_time_in_secs)
-
-
-    # === Cleaning time ===
     show_info(win, join('.', 'messages', 'end.txt'))
 
     # close the EDF data file and put the tracker in idle mode
@@ -324,15 +316,16 @@ def main():
         os.makedirs(dataFolder)
     tk.receiveDataFile(dataFileName, dataFolder + os.sep + dataFileName)
 
+    # clean
     save_beh_results()
     tk.close()
     logging.flush()
     core.quit()
-    win.close()
+    window.close()
     port.close()
 
 
-def run_trial(config, version, fix_stim, left_stim, right_stim, soa, win, arrow_label,question_text, response_clock, trial_index, tk):
+def run_trial(config, fix_stim, left_stim, mask_stim, right_stim, soa, win, arrow_label, question_text, response_clock, trial_index, tk):
     trial_type = random.choice([CorrectStim.LEFT, CorrectStim.RIGHT])
     stim = left_stim if trial_type == CorrectStim.LEFT else right_stim
     stim_name = 'left' if trial_type == CorrectStim.LEFT else 'right'
@@ -376,44 +369,31 @@ def run_trial(config, version, fix_stim, left_stim, right_stim, soa, win, arrow_
 
     win.callOnFlip(tk.sendMessage, "trial_fixation")
     win.callOnFlip(port.writeRegister, FIO1, Triggers.FIXATION)
-
-    for i in range(config['FIXTIME']):  # Fixation cross
+    for i in range(config['FIXTIME']):  # Fixation hexagon
         if i == 2:
             port.writeRegister(FIO1, Triggers.CLEAR)
         fix_stim.draw()
         win.flip()
         check_exit()
 
-    for i in range(config['DELAY']):
+    win.callOnFlip(tk.sendMessage, "trial_stimuli")
+    win.callOnFlip(port.writeRegister, FIO1, Triggers.STIMULI)
+    for i in range(soa):  # Stimulus presentation
+        if i == 2:
+            port.writeRegister(FIO1, Triggers.CLEAR)
+        stim.draw()
+        win.flip()
+        check_exit()
+    
+    win.callOnFlip(tk.sendMessage, "trial_mask")
+    win.callOnFlip(port.writeRegister, FIO1, Triggers.MASK)
+    for i in range(config['MTIME']):  # Mask presentation
+        if i == 2:
+            port.writeRegister(FIO1, Triggers.CLEAR)
+        mask_stim.draw()
         win.flip()
         check_exit()
 
-    win.callOnFlip(tk.sendMessage, "trial_simuli")
-    win.callOnFlip(port.writeRegister, FIO1, Triggers.STIMULI)
-    if version == QuestonVersion.FIRST_SHOWED:
-        for i in range(soa):  # just one stims showed
-            if i == 2:
-                port.writeRegister(FIO1, Triggers.CLEAR)
-            stim.draw()
-            win.flip()
-            check_exit()
-        for i in range(config['TIME']):
-            left_stim.draw()
-            right_stim.draw()
-            win.flip()
-            check_exit()
-    elif version == QuestonVersion.FIRST_HIDDEN:
-        for i in range(config['TIME']):
-            if i == 2:
-                port.writeRegister(FIO1, Triggers.CLEAR)
-            left_stim.draw()
-            right_stim.draw()
-            win.flip()
-            check_exit()
-        for _ in range(soa):  # just one stims showed
-            stim.draw()
-            win.flip()
-            check_exit()
     corr = False  # Used if timeout
     win.callOnFlip(response_clock.reset)
     win.callOnFlip(tk.sendMessage, "trial_reaction")
@@ -431,10 +411,7 @@ def run_trial(config, version, fix_stim, left_stim, right_stim, soa, win, arrow_
             rt = response_clock.getTime()
             break
         check_exit()
-    if version == QuestonVersion.FIRST_HIDDEN:  # Yep, I know, that's ugly
-        corr = not corr
-
-	# Rating Scale
+    # Rating Scale
 
     tk.sendMessage('trial_finished')
     port.writeRegister(FIO1, Triggers.FINISHED)
@@ -451,16 +428,17 @@ def run_trial(config, version, fix_stim, left_stim, right_stim, soa, win, arrow_
     # send over the standard 'TRIAL_RESULT' message to mark the end of trial
     tk.sendMessage('TRIAL_RESULT 0')
 
-
-    ratingScale = visual.RatingScale(win, size = 0.8, noMouse=True, 
-    markerStart = 2, stretch= 1.4, scale="Okre\u015bl swoj\u0105 pewno\u015b\u0107 co do udzielonej odpowiedzi", acceptPreText= 'Wybierz',choices=["\u017badna", "Ma\u0142a", "Du\u017ca", "Ca\u0142kowita"])
+    ratingScale = visual.RatingScale(win, size=0.8, noMouse=True,
+                                     markerStart=2, stretch=1.4, scale="Okre\u015bl swoj\u0105 pewno\u015b\u0107 co do udzielonej odpowiedzi", acceptPreText='Wybierz', choices=["\u017badna", "Ma\u0142a", "Du\u017ca", "Ca\u0142kowita"])
     while ratingScale.noResponse:
         ratingScale.draw()
         win.flip()
     rating = ratingScale.getRating()
     win.flip()
-
-    return corr, rt, stim_name, rating
+    # break + jitter
+    wait_time_in_secs = 1 + random.choice(range(0, 120)) / 60.0
+    core.wait(wait_time_in_secs)
+    return corr, rt, rating
 
 
 if __name__ == '__main__':
